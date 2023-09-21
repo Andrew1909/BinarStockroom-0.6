d04b66a62570a59b69d83cbf5e341fb6e4c1c0d4
Andrew1909 <andey.ryzhov.2002@mail.ru>
2023-09-21 14:05:49 +0300

    first commit

---
+++ Pages/AddDeitalesProductionPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StockroomBinar.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AddDeitalesProductionPage.xaml
+    /// </summary>
+    public partial class AddDeitalesProductionPage : Page
+    {
+        public AddDeitalesProductionPage()
+        {
+            InitializeComponent();
+        }
+
+        private void AddDitalis_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddNameDitalies_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Suports_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Suports_Unchecked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddColordNamePlastic_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void AddNewNameDitales_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# BinarStockroom-0.6
